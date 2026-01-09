f714e9edeef3dca491c51eec0f96c678c9791fb1
Fabrizio <alexandersajor05@gmail.com>
2026-01-09 14:50:26 -0300

    cambio de endpoints V6

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,"view")))
 app.use(cors({
-    origin: ["https://mercado-libre-sajor05.vercel.app/"],
+    origin: ["https://mercado-libre-sajor05.vercel.app"],
     methods: ["GET", "POST"],
     credentials:true
   })
