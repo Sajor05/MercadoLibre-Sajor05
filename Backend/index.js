51e53ecfd92f3be8d0f34f17d32de1bd7db9a7d5
Fabrizio <alexandersajor05@gmail.com>
2026-01-09 18:56:35 -0300

    solucionando serializers

--- Backend/index.js
+++ Backend/index.js
@@ -13,6 +13,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/controllers", express.static(path.join(__dirname, "controllers")));
 app.use(express.static(path.join(__dirname,"view")))
 app.use(cors({
     origin: ["https://mercado-libre-sajor05.vercel.app"],
