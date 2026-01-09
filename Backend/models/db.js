9c1a8013024aa1dcd155aec0d4f7c64bf24691ae
Fabrizio <alexandersajor05@gmail.com>
2026-01-09 17:10:25 -0300

    Refactorizacion de backend

--- Backend/models/db.js
+++ Backend/models/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose"
 
 export const connection = async () => {
+  const db = process.env.MONGO_URI || "mongodb://localhost:27017/mercadolibre_db";
   try {
-    await mongoose.connect("mongodb://localhost/merndb")
+    await mongoose.connect(db)
     console.log("db conectada")
   } catch (error) {
     console.log(error)
